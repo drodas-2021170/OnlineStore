c437fe53ba8df7cf9ef2764279922ed9a1439c1b
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-02-24 11:15:26 -0600

    Se llaman la fucniones addDefaultCategory y defaulttAdmin

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import { initServer } from "./configs/app.js"
 import { config } from "dotenv"
 import { connect } from "./configs/mongo.js"
+import { addDefaultCategory } from "./src/category/category.controller.js"
+import { defaulttAdmin } from "./src/auth/auth.controller.js"
 
 config()
 initServer()
 connect()
+
+addDefaultCategory()
+defaulttAdmin()
