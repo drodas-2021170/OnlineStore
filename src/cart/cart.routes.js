26b2771f59fdf58c12203d90b35ab067ef4c5c6a
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-02-28 11:14:46 -0600

    Se modifica el archivo de rutas de cart

--- src/cart/cart.routes.js
+++ src/cart/cart.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express"
 import { validateToken } from "../../middlewares/validate.jwt.js"
-import { addCart } from "./cart.controller.js"
+import { addCart, getCart } from "./cart.controller.js"
 
 const api = Router()
 
 api.post('/addCart', [validateToken], addCart) 
+api.get('/', getCart)
 
 export default api
