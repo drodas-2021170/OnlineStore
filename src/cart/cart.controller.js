782200ad9f984a20f1b4a3807bcb580a76e6eb80
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-02-27 23:20:42 -0600

    Se modifica el controlador de carrito

--- src/cart/cart.controller.js
+++ src/cart/cart.controller.js
@@ -13,17 +13,27 @@ export const addCart = async(req,res) =>{
         
         let carts = await Cart.findOne({user: req.user.uid}).populate('product', 'name price -_id')
         
+        let productos = carts.product.map(item=>{
+            item.quantity
+        })
+
+        /*carts.product = carts.product.map(item => {
+            if (item._id.toString() === product._id.toString()) {
+                item.quantity += Number(data.quantity); // Sumar cantidad
+            }
+            return item;
+        });*/
+
         if(product.stock === 0) return res.status(404).send({success:false, message:'This product is out of stock'})
         
         if(product.stock < data.quantity) return res.status(404).send({success:false, message:'Cant add more than stock'})
         
-        await Product.findByIdAndUpdate(products, {stock: product.stock - data.quantity})
+        //await Product.findByIdAndUpdate(products, {stock: product.stock - data.quantity})
         
         if(carts){
             carts.product.push(product)
             carts.quantity = carts.quantity + Number(data.quantity)
             carts.subTotal = Number(carts.subTotal) + (product.price * Number(data.quantity))
-            carts.total =  carts.subTotal + (carts.subTotal*0.12)
             let carrito = await carts.save()
             return res.send({success:true, message:'New Product was added to the cart', carrito})
         }else{
