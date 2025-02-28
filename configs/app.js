bffe1ff7670bbd8ccf5cd088a3460615bba12775
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-02-27 23:22:46 -0600

    Se llama la ruta de factura

--- configs/app.js
+++ configs/app.js
@@ -8,6 +8,7 @@ import userRoutes from "../src/user/user.routes.js"
 import categoryRoutes from "../src/category/category.routes.js"
 import productRoutes from "../src/product/product.routes.js"
 import cartRoutes from "../src/cart/cart.routes.js"
+import billRoutes from "../src/bill/bill.routes.js"
 
 const configs = (app) =>{
     app.use(express.json())
@@ -23,6 +24,7 @@ const routes = (app) =>{
     app.use('/v1/category', categoryRoutes)
     app.use('/v1/product', productRoutes)
     app.use('/v1/cart', cartRoutes)
+    app.use('/v1/bill', billRoutes)
 }
 
 export const initServer = async() =>{
