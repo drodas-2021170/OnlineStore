5ff9380bb1b5a09e2f66a262cd47805c5b0a264b
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-02-28 22:12:36 -0600

    Se modifica el controlador de factura

--- src/bill/bill.controller.js
+++ src/bill/bill.controller.js
@@ -4,13 +4,13 @@ import Cart from "../cart/cart.model.js"
 
 export const getBill = async(req, res) =>{
     try {
-        let cart = await Cart.findOne({user: req.user.uid})
+        let cart = await Cart.findOne({user: req.user.uid}).populate('user', 'name surname username')
 
         let bill = new Bill({cart: cart.id})
         if(cart.length === 0) return res.status(404).send({success:false, message:'Procesing Bill'})
             
             bill.total = cart.subTotal + (cart.subTotal *0.12)
-            await Cart.findByIdAndUpdate(cart.id, {status: 'COMPLETED'})
+            //await Cart.findByIdAndUpdate(cart.id, {status: 'COMPLETED'})
             await bill.save()
             return res.send({success:false, message:'Procesing Bill', cart})
         
@@ -18,5 +18,18 @@ export const getBill = async(req, res) =>{
     } catch (err) {
         console.error(err)
         return res.status(500).send({success: false, message:'General Error', err})
+    }
+}
+
+export const getMyBill = async(req,res) =>{
+    try {
+        let cart = await Cart.findOne({user: req.user.uid} )
+        console.log(cart.id)
+
+        let bill = await Bill.find({cart: cart.id})
+        return res.send({success: true, message:'Your bill', bill})
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send({success:false, message:'General Error',err})
     }
 }
