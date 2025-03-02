626f61cfd29a25dcdc7d27a32e9d8913da918861
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-03-01 23:40:06 -0600

    Se agregan nuevas rutas y se modifican las ya existentes

--- src/user/user.routes.js
+++ src/user/user.routes.js
@@ -1,15 +1,15 @@
 import { Router } from "express"
 import { isAdmin, validateToken,isClient} from "../../middlewares/validate.jwt.js"
-import { deleteUser, editRole, updateUser, updateUserClient } from "./user.controller.js"
-import { updateRoleValidate } from "../../middlewares/validators.js"
+import { deleteClient, deleteUser, editRole, updateUser, updateUserClient } from "./user.controller.js"
+import { deleteUserValidate, updateRoleValidate, updateUserValidate } from "../../middlewares/validators.js"
 
 const api = Router()
 //Private Routes ADMIN
 api.put('/updateRole', [validateToken,isAdmin, updateRoleValidate],editRole)
-api.put('/updateUser', [validateToken,isAdmin],updateUser)
-api.put('/deleteUser',[validateToken,isAdmin], deleteUser)
+api.put('/updateUser', [validateToken,isAdmin, updateUserValidate],updateUser)
+api.delete('/deleteUser',[validateToken,isAdmin, deleteUserValidate], deleteUser)
 
 //PRIVATE ROUTES CLIENT
-api.put('/updateClient',[validateToken,isClient], updateUserClient)
-
+api.put('/updateClient',[validateToken,isClient,updateUserValidate], updateUserClient)
+api.delete('/deleteCLIENT', [validateToken], deleteClient)
 export default api
