b1f5428fcf7744f1d3a0a6f20a0a98c74c1004c1
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-03-01 23:36:12 -0600

    Se modifican las rutas existentes con las validaciones

--- src/category/category.routes.js
+++ src/category/category.routes.js
@@ -1,15 +1,16 @@
 import { Router } from "express"
 import { addCategory, deleteCategory, getAll, updateCategory } from "./category.controller.js"
 import { isAdmin, validateToken } from "../../middlewares/validate.jwt.js"
+import { createCategoryValidate, deleteCategoryValidate, updateCategoryValidate } from "../../middlewares/validators.js"
 
 const api = Router()
 
 //PRIVATE ROUTES
-api.post('/addCategory', [validateToken,isAdmin],addCategory)
+api.post('/addCategory', [validateToken,isAdmin , createCategoryValidate],addCategory)
 
 api.get('/', [validateToken,isAdmin], getAll)
 
-api.put('/updateCategory',[validateToken,isAdmin], updateCategory)
+api.put('/updateCategory',[validateToken,isAdmin, updateCategoryValidate], updateCategory)
 
-api.delete('/deleteCategory',[validateToken,isAdmin], deleteCategory)
+api.delete('/deleteCategory',[validateToken,isAdmin,deleteCategoryValidate], deleteCategory)
 export default api
