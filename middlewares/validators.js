36bea5117e4d89fe71437e80e46d35aea5b9a5bd
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-03-01 23:29:03 -0600

    Se agrega mas validacion al proyecto

--- middlewares/validators.js
+++ middlewares/validators.js
@@ -23,8 +23,69 @@ export const updateRoleValidate =[
     validateErrors
 ]
 
-/*export const updateUserValidate =[
+export const updateUserValidate =[
     body('name', 'Name cannot be empty').optional().notEmpty(),
     body('surname', 'Surnanme cannot be empty').optional().notEmpty(),
-    
-]*/
+    body('username', 'Username cannot be empty').optional().notEmpty(),
+    body('email', 'Email cannot be empty').optional().notEmpty(),
+    body('phone', 'Phone cannto be empty').optional().notEmpty(),
+    validateErrors
+]
+
+export const deleteUserValidate =[
+    body('id', 'Introduce an ID for delete the user').notEmpty(),
+    validateErrors
+]
+
+export const createCategoryValidate =[
+    body('name', 'Name canntot be empty').notEmpty(),
+    body('description', 'Description cannot be empty').notEmpty(),
+    validateErrors
+]
+
+export const updateCategoryValidate =[
+    body('name', 'Name canntot be empty').optional().notEmpty(),
+    body('description', 'Description cannot be empty').optional().notEmpty(),
+    validateErrors
+]
+
+export const deleteCategoryValidate =[
+    body('id', 'Introduce an ID for delete the category').notEmpty(),
+    validateErrors
+]
+
+export const createAddValidate =[
+    body('name', 'Name is required').notEmpty(),
+    body('description', 'Description is required').notEmpty(),
+    body('price', 'Price is required').notEmpty(),
+    body('stock', 'Stock is required').notEmpty(),
+    body('category', 'Category is required').notEmpty(),
+    validateErrors
+]
+
+export const getEspecificProductValidate =[
+    body('name', 'Name is required').optional().notEmpty(),
+    body('category', 'Category is required').optional().notEmpty(),
+    validateErrors
+]
+
+export const updateProductValidate =[
+    body('id', 'Id is required').notEmpty(),
+    body('name', 'Name is required').optional().notEmpty(),
+    body('description', 'Description is required').optional().notEmpty(),
+    body('price', 'Price is required').optional().notEmpty(),
+    body('stock', 'Stock is required').optional().notEmpty(),
+    body('category', 'Category is required').optional().notEmpty(),
+    validateErrors
+]
+
+export const deleteProductValidate =[
+    body('id', 'Introduce an ID for delete the Product').notEmpty(),
+    validateErrors
+]
+
+export const addCartValidate =[
+    body('products', 'Introduce the id Product').notEmpty(),
+    body('quantity', 'Quantity is required').notEmpty().isInt({min:1}).withMessage('Please 1 product for min'),
+    validateErrors
+]
