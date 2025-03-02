e1c4c4cd74c8a57246aa4f85c3b22e58aa99ebbf
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-03-01 23:34:01 -0600

    Se modifica la ruta de /addCart con nuevas validaciones

--- src/cart/cart.routes.js
+++ src/cart/cart.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express"
 import { validateToken } from "../../middlewares/validate.jwt.js"
 import { addCart, getCart } from "./cart.controller.js"
+import { addCartValidate } from "../../middlewares/validators.js"
 
 const api = Router()
 
-api.post('/addCart', [validateToken], addCart) 
+api.post('/addCart', [validateToken, addCartValidate], addCart) 
 api.get('/', getCart)
 
 export default api
