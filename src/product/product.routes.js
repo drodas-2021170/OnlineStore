92a6b75b6a696a4efa964476a6c732b454e5178c
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-03-01 23:38:53 -0600

    Se agregan nuevas rutas y se modifica las ya existentes con validaciones

--- src/product/product.routes.js
+++ src/product/product.routes.js
@@ -1,17 +1,20 @@
 import { Router } from "express";
 import { isAdmin, validateToken } from "../../middlewares/validate.jwt.js"
-import { addProduct, getAll, updateProduct,ceroStock, deleteProduct} from "./product.controller.js"
+import { addProduct, getAll, updateProduct,ceroStock, deleteProduct, getEspecificProduct, getMostProduct} from "./product.controller.js"
+import { createAddValidate, deleteProductValidate, getEspecificProductValidate, updateProductValidate } from "../../middlewares/validators.js";
 
 const api = Router()
 
 //PRIVATE ROUTES
-api.post('/addProduct', [validateToken], addProduct)
-
-api.get('/',[validateToken,isAdmin],getAll)
+api.post('/addProduct', [validateToken,isAdmin, createAddValidate], addProduct)
 
 api.get('/getStock',[validateToken,isAdmin], ceroStock)
-api.put('/updatedProduct',[validateToken,isAdmin], updateProduct)
+api.put('/updatedProduct',[validateToken,isAdmin,updateProductValidate], updateProduct)
 
-api.put('/deletedProduct', [validateToken,isAdmin],deleteProduct)
+api.delete('/deletedProduct', [validateToken,isAdmin, deleteProductValidate],deleteProduct)
 
+//PUBLIC ROUTES
+api.get('/',[validateToken],getAll)
+api.get('/getMostProducts', [validateToken], getMostProduct)
+api.get('/getEspecificProduct', [validateToken, getEspecificProductValidate],getEspecificProduct)
 export default api
