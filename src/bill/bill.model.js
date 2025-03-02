5112a007fff28e49fcc7da30720293fc803a6f1c
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-03-01 23:32:05 -0600

    Se modifica el modelo de factura

--- src/bill/bill.model.js
+++ src/bill/bill.model.js
@@ -2,6 +2,23 @@ import { Schema, model } from "mongoose"
 
 const billSchema = Schema(
     {
+        user:{
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        product:[
+            {
+                products:{   
+                    type: Schema.Types.ObjectId,
+                    ref: 'Product',
+                },
+                quantity:{
+                    type:Number,
+                },
+                price:{
+                    type:Number
+                }
+        }],
         cart:{
             type: Schema.Types.ObjectId,
             ref: 'Cart'
