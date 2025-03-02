0a588d2d716c18d15a4c973e2f731986f2711b05
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-03-01 23:32:40 -0600

    Se agregan nuevas rutas y se modifican las ya existenes con las nuevas validaciones

--- src/bill/bill.routes.js
+++ src/bill/bill.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express"
-import { getBill, getMyBill } from "./bill.controller.js"
-import { validateToken } from "../../middlewares/validate.jwt.js"
+import { getBill, getHystorial, getMyBill } from "./bill.controller.js"
+import { isAdmin, validateToken } from "../../middlewares/validate.jwt.js"
 
 const api = Router()
 
-api.post('/getBill',[validateToken], getBill)
+api.post('/getBill',[validateToken, isAdmin], getBill)
 api.get('/',[validateToken],getMyBill)
+api.get('/myHistorial',[validateToken],getHystorial)
 
 export default api
