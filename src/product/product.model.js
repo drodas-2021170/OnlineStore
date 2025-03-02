1dc81cc290f9d2d1c79a631e2f7ab505d1339e72
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-03-01 23:38:15 -0600

    Se modifica el modelo de producto

--- src/product/product.model.js
+++ src/product/product.model.js
@@ -28,6 +28,10 @@ const productSchema = Schema (
         status:{
             type: Boolean,
             default: true
+        },
+        updateAt:{
+            type:Number,
+            default:0
         }
     }
 )
