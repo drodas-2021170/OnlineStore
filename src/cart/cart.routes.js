6087ceb40584e3ecee06a0abf503aed64da42377
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-03-03 11:36:55 -0600

    Se modifica las rutas de carrito

--- src/cart/cart.routes.js
+++ src/cart/cart.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express"
 import { validateToken } from "../../middlewares/validate.jwt.js"
-import { addCart, getCart } from "./cart.controller.js"
+import { addCart, deleteProductCart, getCart } from "./cart.controller.js"
 import { addCartValidate } from "../../middlewares/validators.js"
 
 const api = Router()
 
 api.post('/addCart', [validateToken, addCartValidate], addCart) 
 api.get('/', [validateToken],getCart)
+api.delete('/deleteProdyct',[validateToken],deleteProductCart)
 
 export default api
