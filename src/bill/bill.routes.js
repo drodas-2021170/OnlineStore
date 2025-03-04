90a9d8e46dfbf11932eb59d97c3f32d62021bbba
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-03-03 20:33:47 -0600

    Se agregan nueva ruta

--- src/bill/bill.routes.js
+++ src/bill/bill.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { getBill, getHystorial, getClientBills, updateBill } from "./bill.controller.js"
+import { getBill, getHystorial, getClientBills, updateBill, getAllBills } from "./bill.controller.js"
 import { isAdmin, validateToken } from "../../middlewares/validate.jwt.js"
 
 const api = Router()
@@ -7,7 +7,7 @@ const api = Router()
 api.post('/getBill',[validateToken], getBill)
 api.get('/',[validateToken,isAdmin],getClientBills)
 api.get('/myHistorial',[validateToken],getHystorial)
-
+api.get('/getAllBills',[validateToken, isAdmin], getAllBills)
 api.put('/updateBill',[validateToken,isAdmin], updateBill)
 
 export default api
